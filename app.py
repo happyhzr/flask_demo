11d3345d8405cee4402e3e6fc995fd5135839b05
happy <hzr_bee@163.com>
2018-06-23 16:51:25 +0800

    46

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 import config
+from models import User
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(config)
+db.init_app(app)
 
 
 @app.route('/')
@@ -21,6 +24,23 @@ def login():
 def signup():
     if request.method == 'GET':
         return render_template('signup.html')
+    else:
+        phone = request.form.get('phone')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            return '密码不相等'
+
+        user = User.query.filter(User.phone == phone).first()
+        if user:
+            return '该手机号码已被注册'
+
+        user = User(phone=phone, username=username, password=password1)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
