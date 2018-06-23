433a822d5ca52a463dc1185c0141300f79604d82
happy <hzr_bee@163.com>
2018-06-23 11:02:42 +0800

    41

---
+++ config.py
@@ -0,0 +1,18 @@
+from datetime import timedelta
+from os import urandom
+
+DEBUG = True
+SECRET_KEY = urandom(24)
+# SECRET_KEY = 'Keyborad Cat'
+PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = 'Password.123456'
+HOST = '45.63.52.242'
+PORT = 3306
+DATABASE = 'test'
+
+SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}'.format(
+    DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
