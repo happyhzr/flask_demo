0d6e94f15b27da53a89b1e2bef2d8e2179fae32b
happy <hzr_bee@163.com>
2018-06-23 16:15:09 +0800

    43

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import config
 
@@ -9,6 +9,12 @@ app.config.from_object(config)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
 
 
 if __name__ == '__main__':
