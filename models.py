e3383910a85246b9911dcae1340d18a9ba1d7d81
happy <hzr_bee@163.com>
2018-06-23 16:37:48 +0800

    45

--- models.py
+++ models.py
@@ -1,2 +1,9 @@
 from exts import db
 
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    phone = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
